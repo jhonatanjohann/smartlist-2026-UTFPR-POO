ffa17ab92bf2adfb022a5c17b2fee902262537e6
jhonatanjohann <jhon8gf@gmail.com>
2026-08-20 21:43:57 -0300

    Aula-01; 20/08/2026; Iniciando o projeto e revisando o conteudo.

---
+++ src/main/java/br/com/view/ListaDeComprasView.java
@@ -0,0 +1,11 @@
+package br.com.view;
+
+import java.util.Scanner;
+
+public class ListaDeComprasView {
+    private Scanner scanner;
+
+    public ListaDeComprasView() {
+        scanner = new Scanner(System.in);
+    }
+}
